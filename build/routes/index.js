df28ec4245b9a2b443378692756828b98d79a6d9
srgrcp <srgrcp@gmail.com>
2021-03-11 17:15:42 -0500

    .gitignore build

--- build/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var indexRouter = express_1.Router();
-indexRouter.get('/', function (_, res) {
-    res.json({ title: 'Express' });
-});
-exports.default = indexRouter;
-//# sourceMappingURL=index.js.map
